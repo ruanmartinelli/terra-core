800e2f327af01292d320e9e7014b6dd56968555f
Ruan Martinelli <martinelliruan@gmail.com>
2017-02-01 09:38:28 -0200

    add subscriber

--- src/app/event/event-bootstrap.js
+++ src/app/event/event-bootstrap.js
@@ -1,5 +1,9 @@
+const zmq = require('zmq')
+const subscriber = zmq.socket('sub')
+
 const bootstrap = (app) => {
 
+    // socket io
     const server = require('http').createServer(app)
     const io = require('socket.io')(server)
 
@@ -8,6 +12,21 @@ const bootstrap = (app) => {
     setTimeout(() => io.emit('message', 'hello'), 5000)
 
     server.listen(3000)
+
+    // zmq
+    subscriber.subscribe('test')
+    subscriber.connect('tcp://localhost:5563')
+    subscriber.monitor(500, 0)
+
+    subscriber.on('subscribe', (fd, ep) => {
+        console.log('-- connected to publisher')
+    })
+
+    subscriber.on('message', (channel, message) => {
+        console.log(' -- new message:', channel.toString(), message.toString())
+    })
 }
+
+
 
 module.exports = bootstrap
