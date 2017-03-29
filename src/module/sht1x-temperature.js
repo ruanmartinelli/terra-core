37984969dd9fdfb07617a1c1d0042592cd57b9ee
Ruan Martinelli <martinelliruan@gmail.com>
2017-03-29 20:42:31 -0300

    add sht1x convert function

---
+++ src/module/sht1x-temperature.js
@@ -0,0 +1,12 @@
+/**
+ * converts raw data to temperature in SHT1x motes
+ * see chapter 4.3 in https://www.sensirion.com/fileadmin/user_upload/customers/sensirion/Dokumente/2_Humidity_Sensors/Sensirion_Humidity_Sensors_SHT1x_Datasheet_V5.pdf
+ * 
+ */
+module.exports = (value) => {
+
+    const d1 = -39.6
+    const d2 = 0.04
+
+    return (value * d2) + d1 // celsius
+}
