ffc6e811c5e8b6fa97d2aa76a04250fb5002735a
Ruan Martinelli <martinelliruan@gmail.com>
2017-04-12 19:26:02 -0300

    match ids from config

--- src/app/event/event-listener.js
+++ src/app/event/event-listener.js
@@ -38,7 +38,7 @@ const initSimulation = (app) => {
     setInterval(() => {
         const fake_event = {
             port: random(9002, 9005),
-            source: random(19, 39),
+            source: random(10, 34), // should match ids from config file
             gateway_time: new Date().getTime() - random(400, 500),
             d8: [counter++, 0, 0, 0],
             d16: [random(400, 470), 0, 0, 0],
